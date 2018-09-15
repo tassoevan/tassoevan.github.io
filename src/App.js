7db18d2307841e8f2e31ca614a1497a8ffdb1437
Tasso Evangelista <tasso@tassoevan.me>
2018-09-15 00:18:38 -0300

    Add layout decorators

--- src/App.js
+++ src/App.js
@@ -1,48 +1,70 @@
 import React, { Component } from 'react';
+import regularIcons from '@fortawesome/fontawesome-free/sprites/regular.svg';
+import brandsIcons from '@fortawesome/fontawesome-free/sprites/brands.svg';
 import './App.css';
+import icon from './icon.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App hero is-fullheight">
-        <div class="hero-body">
-          <div class="container has-text-centered">
-            <h1 class="title">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <div>
+              <img className="App__icon" src={icon} alt="" />
+            </div>
+
+            <h1 className="title">
               Tasso Evangelista
             </h1>
-            <h2 class="subtitle">
+            <h2 className="subtitle">
               Front-end Engineer
             </h2>
           </div>
         </div>
 
-        <div class="hero-foot">
-          <nav class="tabs is-fullwidth">
-            <div class="container">
+        <div className="hero-foot">
+          <nav className="tabs is-fullwidth">
+            <div className="container">
               <ul>
                 <li>
                   <a href="mailto:tasso@tassoevan.me">
-                    E-mail
+                    <svg className="icon">
+                      <use style={{ fill: 'currentColor' }} xlinkHref={`${ regularIcons }#envelope`}></use>
+                    </svg>
+                    <span className="is-hidden-touch is-hidden-desktop-only">tasso@tassoevan.me</span>
                   </a>
                 </li>
                 <li>
-                  <a href="http://twitter.com/tassoevan" rel="noopener noreferrer" target="_blank">
-                    Twitter
+                  <a href="https://twitter.com/tassoevan" rel="noopener noreferrer" target="_blank">
+                    <svg className="icon">
+                      <use style={{ fill: 'currentColor' }} xlinkHref={`${ brandsIcons }#twitter`}></use>
+                    </svg>
+                    <span className="is-hidden-touch is-hidden-desktop-only">@tassoevan</span>
                   </a>
                 </li>
                 <li>
-                  <a href="http://github.com/tassoevan" rel="noopener noreferrer" target="_blank">
-                    GitHub
+                  <a href="https://github.com/tassoevan" rel="noopener noreferrer" target="_blank">
+                    <svg className="icon">
+                      <use style={{ fill: 'currentColor' }} xlinkHref={`${ brandsIcons }#github`}></use>
+                    </svg>
+                    <span className="is-hidden-touch is-hidden-desktop-only">@tassoevan</span>
                   </a>
                 </li>
                 <li>
-                  <a href="http://bitbucket.org/tassoevan" rel="noopener noreferrer" target="_blank">
-                    Bitbucket
+                  <a href="https://bitbucket.org/tassoevan" rel="noopener noreferrer" target="_blank">
+                    <svg className="icon">
+                      <use style={{ fill: 'currentColor' }} xlinkHref={`${ brandsIcons }#bitbucket`}></use>
+                    </svg>
+                    <span className="is-hidden-touch is-hidden-desktop-only">@tassoevan</span>
                   </a>
                 </li>
                 <li>
-                  <a href="http://linkedin.com/in/tassoevan" rel="noopener noreferrer" target="_blank">
-                    LinkedIn
+                  <a href="https://linkedin.com/in/tassoevan" rel="noopener noreferrer" target="_blank">
+                    <svg className="icon">
+                      <use style={{ fill: 'currentColor' }} xlinkHref={`${ brandsIcons }#linkedin`}></use>
+                    </svg>
+                    <span className="is-hidden-touch is-hidden-desktop-only">tassoevan</span>
                   </a>
                 </li>
               </ul>
