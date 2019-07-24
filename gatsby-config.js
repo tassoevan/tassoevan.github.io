ed824c6d008d820c9a675e29df45a0353e84f33f
Tasso Evangelista <tasso@tassoevan.me>
2019-07-23 23:14:30 -0300

    Add Gatsby scaffolding

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: `Tasso Evangelista`,
+    description: `Front-end Engineer`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Tasso Evangelista`,
+        description: `Front-end Engineer`,
+        short_name: `Tasso Evangelista`,
+        start_url: `/`,
+        background_color: `#91d2fa`,
+        theme_color: `#91d2fa`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
