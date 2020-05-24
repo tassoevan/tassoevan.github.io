f12efb2356830c6512e403014d36ceace7d83ee1
Tasso Evangelista <tasso@tassoevan.me>
2020-05-24 00:17:53 -0300

    Add post list on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,32 @@ import 'normalize.css';
 import 'sakura.css';
 
 import { useSiteMetadata } from '../hooks/useSiteMetadata';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import SEO from '../components/SEO';
 
 const IndexPage = () => {
   const { title, description } = useSiteMetadata();
+
+  const data = useStaticQuery(graphql`
+    query SITE_INDEX_QUERY {
+      allMdx(
+        sort: { fields: frontmatter___date, order: DESC }
+        filter: { frontmatter: { published: { eq: true } } }
+      ) {
+        nodes {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            title
+            date
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `);
 
   return (
     <>
@@ -18,6 +39,15 @@ const IndexPage = () => {
         </Link>
         <p>{description}</p>
       </header>
+      {data.allMdx.nodes.map(({ frontmatter, fields, excerpt }) => (
+        <>
+          <h2>
+            <Link to={fields.slug}>{frontmatter.title}</Link>
+          </h2>
+          <p>{frontmatter.date}</p>
+          <p>{excerpt}</p>
+        </>
+      ))}
     </>
   );
 };
