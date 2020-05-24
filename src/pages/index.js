0854c2088475cc9282d9599245ef92c540e04c42
Tasso Evangelista <tasso@tassoevan.me>
2020-05-23 23:15:24 -0300

    Add sakura.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import 'normalize.css';
+import 'sakura.css';
 
-import Hero from '../components/Hero';
-import SEO from '../components/SEO';
 import { useSiteMetadata } from '../hooks/useSiteMetadata';
 
 const IndexPage = () => {
   const { title, description } = useSiteMetadata();
 
   return (
-    <Hero>
-      <SEO title='Home' />
+    <>
       <h1>{title}</h1>
       <p>{description}</p>
-    </Hero>
+    </>
   );
 };
 
