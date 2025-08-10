2810cad8ce5ad9fefa76c13e0387be9baff9a97f
Tasso <tasso@tassoevan.me>
2025-08-10 17:55:49 -0300

    chore: Add scaffolding files

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import "../style.css";
+import "../client";
+
+function Index() {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <title>Tasso Evangelista - Personal Website</title>
+      </head>
+      <body>
+        <h1>Tasso Evangelista</h1>
+        <p>
+          Welcome to my personal <del>website</del> experiment!
+        </p>
+      </body>
+    </html>
+  );
+}
+
+export default Index;
