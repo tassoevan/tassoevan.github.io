309cec3954e57a9066201cc55214fa033ff1ef2c
Tasso <tasso@tassoevan.me>
2025-09-20 23:02:38 -0300

    fix: Adjust font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from 'next';
-import { Open_Sans, Roboto_Mono } from 'next/font/google';
 import './globals.css';
-
-const openSans = Open_Sans({
-  variable: '--font-open-sans',
-  subsets: ['latin'],
-});
-
-const robotoMono = Roboto_Mono({
-  variable: '--font-roboto-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Tasso Evangelista',
@@ -24,11 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${openSans.variable} ${robotoMono.variable} ${openSans.className} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
