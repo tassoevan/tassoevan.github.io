8c0574fe4ef9a84767c5757276492eecd65602d7
Tasso Evangelista Pinto <tasso@tassoevan.me>
2025-10-25 00:29:53 -0300

    feat: Migrate to Vite

---
+++ src/components/ExternalLink.tsx
@@ -0,0 +1,13 @@
+import { forwardRef } from "react";
+import type { LinkProps } from "./Link";
+import Link from "./Link";
+
+export type ExternalLinkProps = LinkProps;
+
+export default forwardRef<HTMLAnchorElement, ExternalLinkProps>(
+  function ExternalLink(props, ref) {
+    return (
+      <Link ref={ref} target="_blank" rel="noopener noreferrer" {...props} />
+    );
+  }
+);
